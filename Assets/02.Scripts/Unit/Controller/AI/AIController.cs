627fe0c8d53c31bd10de29c8d3ce695ac3721678
khh7052 <59150989+khh7052@users.noreply.github.com>
2023-05-18 21:35:55 +0900

    2023.05.18(09:35)
    NavmeshAgent 생성시 버그 ("SetDestination" can only be called on an active agent that has been placed on a NavMesh.) 해결

--- Assets/02.Scripts/Unit/Controller/AI/AIController.cs
+++ Assets/02.Scripts/Unit/Controller/AI/AIController.cs
@@ -173,7 +173,7 @@ public class AIController : Controller
     {
         if (agent.isOnNavMesh) return;
 
-        agent.transform.position = agent.pathEndPosition;
+        agent.Warp(agent.nextPosition);
     }
 
     public virtual void TargetUpdate()
